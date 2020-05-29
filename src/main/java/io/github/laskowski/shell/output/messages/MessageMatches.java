574c36a7a3297e08c9e477a9b550a0dc805c0c84
rafal_laskowski <rafal.laskowski@intive.com>
2020-05-29 06:13:45 +0200

    MessageMatches implements MessageExclusionStrategy

--- src/main/java/io/github/laskowski/shell/output/messages/MessageMatches.java
+++ src/main/java/io/github/laskowski/shell/output/messages/MessageMatches.java
@@ -2,7 +2,7 @@ package io.github.laskowski.shell.output.messages;
 
 import io.github.laskowski.shell.output.service.ServiceReadyPredicate;
 
-public class MessageMatches implements ServiceReadyPredicate, ServiceErrorPredicate {
+public class MessageMatches implements ServiceReadyPredicate, ServiceErrorPredicate, MessageExclusionStrategy {
     private final String expected;
 
     public MessageMatches(String expected) {
